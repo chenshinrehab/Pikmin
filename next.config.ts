92657a7324cb8c7def8b4141c55e6c5714829552
jimmy <patrioticman123@gmail.com>
2026-04-06 18:05:30 +0800

    chore: 移除 PWA 相關套件，回歸標準網頁版配置

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    skipWaiting: true,
-    clientsClaim: true,
-  },
-});
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // 加入以下設定，解決 Webpack 模式下 CSS 消失的問題
-  webpack: (config) => {
-    return config;
-  },
+  /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
